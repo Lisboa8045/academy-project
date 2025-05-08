025f280ad37a9099785b3e294eaa35bdd0e4b28d
Diogo Sousa <48392665+Calmskyy@users.noreply.github.com>
2025-05-08 12:16:00 +0100

    Add service + tag repositories

---
+++ src/main/java/com/academy/Repositories/ServiceRepository.java
@@ -0,0 +1,7 @@
+package com.academy.Repositories;
+
+import com.academy.Models.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+}
---
+++ src/main/java/com/academy/Repositories/ServiceTagRepository.java
@@ -0,0 +1,7 @@
+package com.academy.Repositories;
+
+import com.academy.Models.ServiceTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceTagRepository extends JpaRepository<ServiceTag, Integer> {
+}
