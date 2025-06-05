f82f986585368c719d8bded79e8b07d6273a6ae3
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-05 15:44:58 +0100

    Added test for GetAllConfigs()

---
+++ src/test/java/com/academy/global_configurations/GlobalConfigurationTests.java
@@ -0,0 +1,80 @@
+package com.academy.global_configurations;
+
+import com.academy.dtos.global_configuration.GlobalConfigurationResponseDTO;
+import com.academy.models.global_configuration.GlobalConfiguration;
+import com.academy.models.global_configuration.GlobalConfigurationTypeEnum;
+import com.academy.repositories.GlobalConfigurationRepository;
+import com.academy.services.GlobalConfigurationService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension.class)
+class GlobalConfigurationTests {
+
+    private final GlobalConfigurationService globalConfigurationService;
+    private final GlobalConfigurationRepository globalConfigurationRepository;
+
+    @Autowired
+    public GlobalConfigurationTests(GlobalConfigurationService globalConfigurationService,
+                                    GlobalConfigurationRepository globalConfigurationRepository) {
+        this.globalConfigurationService = globalConfigurationService;
+        this.globalConfigurationRepository = globalConfigurationRepository;
+    }
+
+    @BeforeEach
+    void setUp() {
+        GlobalConfiguration config = new GlobalConfiguration();
+        config.setConfigKey("configKey_string");
+        config.setConfigValue("configValue");
+        config.setConfigType(GlobalConfigurationTypeEnum.STRING);
+        this.globalConfigurationRepository.save(config);
+
+        config = new GlobalConfiguration();
+        config.setConfigKey("configKey_int");
+        config.setConfigValue("123");
+        config.setConfigType(GlobalConfigurationTypeEnum.INT);
+        this.globalConfigurationRepository.save(config);
+
+        config = new GlobalConfiguration();
+        config.setConfigKey("configKey_boolean");
+        config.setConfigValue("true");
+        config.setConfigType(GlobalConfigurationTypeEnum.BOOLEAN);
+        this.globalConfigurationRepository.save(config);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.globalConfigurationRepository.deleteAll();
+    }
+
+    @Test
+    void testGetAllGlobalConfigurations() {
+        List<GlobalConfigurationResponseDTO> configs = this.globalConfigurationService.getAllConfigs();
+        GlobalConfigurationResponseDTO config = configs.get(0);
+        assertEquals("configKey_string", config.configKey());
+        assertEquals("configValue", config.configValue());
+        assertEquals(GlobalConfigurationTypeEnum.STRING, config.configType());
+
+        config = configs.get(1);
+        assertEquals("configKey_int", config.configKey());
+        assertEquals("123", config.configValue());
+        assertEquals(GlobalConfigurationTypeEnum.INT, config.configType());
+
+        config = configs.get(2);
+        assertEquals("configKey_boolean", config.configKey());
+        assertEquals("true", config.configValue());
+        assertEquals(GlobalConfigurationTypeEnum.BOOLEAN, config.configType());
+    }
+}
