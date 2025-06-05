91cb9de74618da0919ecebab667fc22c3abc0b22
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-05 14:47:33 +0100

    Added populateGlobalConfigurations to CommandLineRunner.

--- src/main/java/com/academy/AcademyApplication.java
+++ src/main/java/com/academy/AcademyApplication.java
@@ -1,15 +1,7 @@
 package com.academy;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @SpringBootApplication
 public class AcademyApplication {
@@ -24,10 +16,12 @@ public class AcademyApplication {
 			Resource resource = new ClassPathResource("sql-scripts/populateRoles.sql");
 			Resource resource2 = new ClassPathResource("sql-scripts/populateMembers.sql");
 			Resource resource3 = new ClassPathResource("sql-scripts/populateServices.sql");
+			Resource resource4 = new ClassPathResource("sql-scripts/populateGlobalConfigurations.sql");
 			try (Connection conn = dataSource.getConnection()) {
 				ScriptUtils.executeSqlScript(conn, resource);
 				ScriptUtils.executeSqlScript(conn, resource2);
 				ScriptUtils.executeSqlScript(conn, resource3);
+				ScriptUtils.executeSqlScript(conn, resource4);
 			}
 			System.out.println("Populated database");
 		};
