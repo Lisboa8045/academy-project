8cdcf93fdebc67bd7217e23e8388daab350ff4de
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-09 19:15:37 +0100

    inserted profile button in app-header

--- frontend/src/app/header/app-header.component.ts
+++ frontend/src/app/header/app-header.component.ts
@@ -2,11 +2,12 @@ import {Component, inject, Input, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import {AuthStore} from '../auth/auth.store';
+import {ProfileButtonComponent} from './profile-button/profile-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ProfileButtonComponent],
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.css']
 })
