b2c5440072ea12c0584af1ef7209e0aa57fa7d0b
Diogo Sousa <48392665+Calmskyy@users.noreply.github.com>
2025-06-11 16:03:44 +0100

    added search bar to header

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {AuthComponent} from './auth/auth.component';
 import {ProfileComponent} from './profile/profile.component';
 import {UnauthorizedComponent} from './unauthorized.component';
+import {ServiceListComponent} from './service/service-list.component';
 
 export const routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'unauthorized', component: UnauthorizedComponent },
+  { path: 'services', component: ServiceListComponent },
 ];
--- frontend/src/app/header/app-header.component.ts
+++ frontend/src/app/header/app-header.component.ts
@@ -1,16 +1,35 @@
-import {Component, inject, Input, signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import {AuthStore} from '../auth/auth.store';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.css']
 })
 export class AppHeaderComponent {
+  searchQuery: string = '';
   readonly username = inject(AuthStore).username;
 
+  constructor(private router: Router) {}
+
+  onSearch(): void {
+    const query = this.searchQuery.trim();
+
+    if (query.length > 100) {
+      this.searchQuery = query.slice(0, 100);
+    } else {
+      this.searchQuery = query;
+    }
+
+    if (this.searchQuery) {
+      this.router.navigate(['/services'], {
+        queryParams: { q: this.searchQuery },
+      });
+    }
+  }
 }
