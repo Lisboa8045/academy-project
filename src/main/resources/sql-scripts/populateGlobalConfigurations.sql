542384c1070c153d69c90b4fd103ec9793c9c354
AdrianoQueiroz <adriano.l.a.queiroz@gmail.com>
2025-06-12 15:44:58 +0100

    Corrigido populate Global Configurations

--- src/main/resources/sql-scripts/populateGlobalConfigurations.sql
+++ src/main/resources/sql-scripts/populateGlobalConfigurations.sql
@@ -2,6 +2,6 @@
 INSERT IGNORE INTO `global_configuration`
 (id, config_key, config_value, config_type, created_at, updated_at)
 values
-    (1, 'confirmation_token_expiry_minutes', 60)
-    (2, 'email', 'academy.project.do.not.reply@gmail.com')
-    (3, 'password', 'hehixeicdqmkvjjo')
+    (1, 'confirmation_token_expiry_minutes', 60, 'INT', NOW(), NOW())
+    (2, 'email', 'academy.project.do.not.reply@gmail.com', 'STRING', NOW(), NOW())
+    (3, 'password', 'hehixeicdqmkvjjo', 'STRING', NOW(), NOW())
