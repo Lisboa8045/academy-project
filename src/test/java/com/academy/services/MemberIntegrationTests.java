26a40157029b8193125b1ae6b3fd302d34497d59
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-12 17:00:32 +0100

    Removed DirtiesContext from test as it is very slow.

--- src/test/java/com/academy/services/MemberIntegrationTests.java
+++ src/test/java/com/academy/services/MemberIntegrationTests.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 @SpringBootTest
 @Transactional
 @ExtendWith(SpringExtension.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MemberIntegrationTests {
 
     private final MemberService memberService;
