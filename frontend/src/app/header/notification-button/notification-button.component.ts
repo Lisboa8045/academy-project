116cef68e4593cc7f4349f5c7ab0d63c8d9b78a1
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-16 20:25:26 +0100

    Added notification-list.component

--- frontend/src/app/header/notification-button/notification-button.component.ts
+++ frontend/src/app/header/notification-button/notification-button.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {NotificationListComponent} from './notification-list/notification-list.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-notification-button',
-  imports: [],
+  imports: [
+    NotificationListComponent,
+    NgIf
+  ],
   templateUrl: './notification-button.component.html',
   styleUrl: './notification-button.component.css'
 })
 export class NotificationButtonComponent {
+  showList = signal(false);
 
+  toggleList() {
+    this.showList.set(!this.showList());
+  }
+
+  closeList() {
+    this.showList.set(false);
+  }
 }
