5956f0e611208092ae3ab347071294b500dd65d8
BCorreia02 <84812282+BCorreia02@users.noreply.github.com>
2025-06-23 16:15:26 +0100

    Reapply "Merge branch 'main' into landing-page"
    This reverts commit f752dfe525d80da27e22fb8e1b97fdcfa2182c06.

--- src/main/java/com/academy/models/Availability.java
+++ src/main/java/com/academy/models/Availability.java
@@ -1,36 +1,28 @@
 package com.academy.models;
 
+import com.academy.models.shared.BaseEntity;
+import com.academy.models.member.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@ToString(exclude="member")
+@ToString(callSuper = true, exclude="member")
 @Entity
 @Table(name = "availability")
 
-public class Availability {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
-    private Long id;
+public class Availability extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "member_id")
@@ -46,11 +38,4 @@ public class Availability {
     @Column(name = "end_date_time")
     private LocalDateTime endDateTime;
 
-    @CreationTimestamp
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 }
--- src/main/resources/sql-scripts/populateRoles.sql
+++ src/main/resources/sql-scripts/populateRoles.sql
@@ -1,7 +1,7 @@
 -- Populates role table
 INSERT IGNORE INTO `role`
-(id, name)
+(id, name, updated_at, created_at, updated_by, created_by)
 values
-    (1, 'ADMIN'),
-    (2, 'CLIENT'),
-    (3, 'WORKER');
+    (1, 'ADMIN', NOW(), NOW(), 'system', 'system'),
+    (2, 'CLIENT', NOW(), NOW(), 'system', 'system'),
+    (3, 'WORKER', NOW(), NOW(), 'system', 'system');
