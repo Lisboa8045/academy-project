412a0293652ec1e3b9ae80b97918877795d428e2
FlávioMartins <flaviozmiguel27@gmail.com>
2025-06-25 17:48:11 +0100

    App footer shows

--- src/main/java/com/academy/controllers/AuthController.java
+++ src/main/java/com/academy/controllers/AuthController.java
@@ -15,7 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -70,6 +74,5 @@ public class AuthController {
 
         return ResponseEntity.ok(Map.of("username", username));
     }
-
 
 }
