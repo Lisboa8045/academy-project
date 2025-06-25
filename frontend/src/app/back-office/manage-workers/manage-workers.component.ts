20ce678776ef1c3503cda6fa9a9ca4781385753c
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-25 15:51:48 +0100

    Added route and component for Manage Workers Page.

---
+++ frontend/src/app/back-office/manage-workers/manage-workers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-workers',
+  imports: [],
+  templateUrl: './manage-workers.component.html',
+  styleUrl: './manage-workers.component.css'
+})
+export class ManageWorkersComponent {
+
+}
