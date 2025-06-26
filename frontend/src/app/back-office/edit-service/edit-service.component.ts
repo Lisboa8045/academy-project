be55f7b66aee5205e37912a2691568b0975081bc
FranciscoMarques <f.lisboamarques@gmail.com>
2025-06-26 10:52:38 +0100

    removed unused imports

--- frontend/src/app/back-office/edit-service/edit-service.component.ts
+++ frontend/src/app/back-office/edit-service/edit-service.component.ts
@@ -1,13 +1,10 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {LoadingComponent} from '../../loading/loading.component';
-import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-edit-service',
   imports: [
-    LoadingComponent,
-    NgIf,
-    NgForOf
+    LoadingComponent
   ],
   templateUrl: './edit-service.component.html',
   styleUrl: './edit-service.component.css'
