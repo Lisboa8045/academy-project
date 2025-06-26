7af49b92cc3ecc114268fbfdac09053471c89503
FlávioMartins <flaviozmiguel27@gmail.com>
2025-06-26 17:01:38 +0100

    Commit ServiceImages extends BaseEntity

--- src/main/java/com/academy/models/service/ServiceImages.java
+++ src/main/java/com/academy/models/service/ServiceImages.java
@@ -1,11 +1,9 @@
 package com.academy.models.service;
 
+import com.academy.models.shared.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -16,11 +14,8 @@ import lombok.Setter;
 @Table(name="service_images")
 @Getter
 @Setter
-public class ServiceImages {
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+public class ServiceImages extends BaseEntity {
+
 
     @Column(name="image")
     private String image;
