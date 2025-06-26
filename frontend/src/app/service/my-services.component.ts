1fd99063899cc9c9568a6119571b94d1db552285
VascoAraújo <vasconparaujo@gmail.com>
2025-06-26 14:40:45 +0100

    Main Fixes

--- frontend/src/app/service/my-services.component.ts
+++ frontend/src/app/service/my-services.component.ts
@@ -1,14 +1,10 @@
 import {Component, effect, OnInit, signal} from '@angular/core';
-import {ServiceModel} from './service.model';
-import {ServiceApiService, PagedServicesResponse} from '../shared/service-api.service';
-import {LoadingComponent} from '../loading/loading.component';
-import {DatePipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-my-services',
   templateUrl: './my-services.component.html',
   styleUrls: ['./my-services.component.css'],
-  imports: [LoadingComponent, NgForOf, DatePipe]
+  imports: []
 })
 export class MyServicesComponent implements OnInit{
 
