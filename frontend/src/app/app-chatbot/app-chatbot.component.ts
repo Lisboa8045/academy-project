d0020824a79307e4db85ae91c0ccc53037c3e5b2
AdrianoQueiroz <adriano.l.a.queiroz@gmail.com>
2025-08-28 13:11:15 +0100

    Chatbot controller and url in environment properties

--- frontend/src/app/app-chatbot/app-chatbot.component.ts
+++ frontend/src/app/app-chatbot/app-chatbot.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {createChat} from '@n8n/chat';
+import {environment} from '../../enviroments/environments';
 
 @Component({
   selector: 'app-chatbot',
@@ -10,7 +11,7 @@ export class AppChatbotComponent implements OnInit{
 
     ngOnInit(): void {
       createChat({
-        webhookUrl: 'https://adrianoqueiroz.app.n8n.cloud/webhook/e985d15f-b2f6-456d-be15-97e0b1544a40/chat'
+        webhookUrl: environment.chatbotUrl
       });
     }
 
