9050e50b4c91fcd58534809a6156c0f1d07d00c6
FranciscoMarques <f.lisboamarques@gmail.com>
2025-09-28 20:45:29 +0100

    Adicionar nome e favicon custom.

--- frontend/src/app/info-pages/about/about.component.ts
+++ frontend/src/app/info-pages/about/about.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {environment} from '../../../enviroments/environments';
 
 @Component({
   selector: 'app-about',
@@ -10,4 +11,5 @@ import {CommonModule} from '@angular/common';
 export class AboutComponent {
 
   readonly usernames = ['Adriano-Queiroz', 'BCorreia02', 'Calmskyy', 'FlavioMiguel27', 'Lisboa8045', 'Shrimpo22'];
+  protected readonly environment = environment;
 }
